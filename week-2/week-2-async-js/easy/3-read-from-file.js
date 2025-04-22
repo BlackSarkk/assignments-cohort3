9e3169d75aeeab7df7b2f5768a2e1a256a0a2037
ObscureDev <121737321+BlackSarkk@users.noreply.github.com>
2025-04-22 09:25:37 +0000

    added : week-2-async.js : easy: 4 added

--- week-2/week-2-async-js/easy/3-read-from-file.js
+++ week-2/week-2-async-js/easy/3-read-from-file.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
 
+const expensiveTask = () => {
+    let m = 0
+    for(let i = 0; i<1000000000; i++){
+        m += i
+    }
+    console.log(`Expensive operation done`)
+}
+
+expensiveTask()
+
+
+
 const read = () => {
-    console.log(fs.readFileSync('test1.txt', 'utf-8'));
+    console.log(fs.readFile('test1.txt', 'utf-8'));
 };
 
 read();
+
+
+/**
+ const read = () => {
+    fs.readFile('test1.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(data);
+    });
+};
+ */
