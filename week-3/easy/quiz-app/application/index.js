8c52d432a8d735bf99b924278ab23d8a10f9ee52
rahul <droncer22446688@gmail.com>
2025-04-23 11:42:01 +0530

    "week-3: easy quizApp done"

--- week-3/easy/quiz-app/application/index.js
+++ week-3/easy/quiz-app/application/index.js
@@ -1,52 +1,51 @@
-// // const fs = require('fs').promises
-// import { quizData } from "./data.js"
+import { quizData } from "./data.js";
 
+let currentIndex = 0;
+let totalQuest = 0;
+let Score = 0;
+let currentVal = quizData[currentIndex];
 
-// let currentIndex = 0
-// let currentVal = quizData[currentIndex]
-// const next = () => {
-//     const selected = document.querySelector('input[name="answer"]:checked');
-  
-//     // if (!selected) {
-//     //   alert("Please select an option before submitting!");
-//     //   return;
-//     // }
-  
-//     const answer = selected.value;
-//     if (answer == quizData[currentIndex].correct) {
-//       score++;
-//     }
-  
-//     currentIndex++;
-//     if (currentIndex < quizData.length) {
-//       setPage();
-//     } else {
-//       showResult();
-//     }
-  
-//   };
+const setPage = () => {
+    if (currentIndex < quizData.length) {
+        currentVal = quizData[currentIndex];
+        document.getElementById('qn').innerHTML = currentVal.question;
+        document.getElementById('op1').innerHTML = `<input type="radio" name="color" value="a"> ${currentVal.a}`;
+        document.getElementById('op2').innerHTML = `<input type="radio" name="color" value="b"> ${currentVal.b}`;
+        document.getElementById('op3').innerHTML = `<input type="radio" name="color" value="c"> ${currentVal.c}`;
+        document.getElementById('op4').innerHTML = `<input type="radio" name="color" value="d"> ${currentVal.d}`;
+    }
+};
 
-// const setPage = () => {
-//     if (currentIndex < quizData.length) {
-//         currentVal = quizData[currentIndex];
+const next = () => {
+    const selected = document.querySelector('input[name="color"]:checked');
 
-//         // Update question
-//         document.getElementById("qn").innerHTML = currentVal.question;
+    if (!selected) {
+        alert("Please select an option before submitting!");
+        return;
+    }
 
-//         // Update options
-//         document.getElementById("op1").innerHTML = `<input type="radio" name="color" /> ${currentVal.a}`;
-//         document.getElementById("op2").innerHTML = `<input type="radio" name="color" /> ${currentVal.b}`;
-//         document.getElementById("op3").innerHTML = `<input type="radio" name="color" /> ${currentVal.c}`;
-//         document.getElementById("op4").innerHTML = `<input type="radio" name="color" /> ${currentVal.d}`;
-//     } else {
-//         document.getElementById("qn").innerHTML = "Quiz Completed!";
-//         document.querySelector("form").style.display = "none";
-//     }
-// }
+    const answer = selected.value;
 
+    if (answer === quizData[currentIndex].correct) {
+        Score++;
+    }
 
-// window.next = next;
+    totalQuest++;
+    currentIndex++;
 
-// window.onload = () => {
-//     setPage();
-// };
+    if (currentIndex < quizData.length) {
+        setPage();
+    } else {
+        document.getElementById('qn').innerHTML = `Quiz Finished. Your score is: ${Score}/${totalQuest}`;
+        document.getElementById('op1').innerHTML = ``;
+        document.getElementById('op2').innerHTML = ``;
+        document.getElementById('op3').innerHTML = ``;
+        document.getElementById('op4').innerHTML = ``;
+    }
+};
+
+window.next = next;
+
+window.onload = () => {
+    setPage();
+};
